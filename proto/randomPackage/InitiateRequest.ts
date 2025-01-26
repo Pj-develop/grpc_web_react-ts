877642aa8d28b0e45c97dfb7dccc5b6ede89e1af
Priyanshu Jha <priyanshu.k4444@gmail.com>
2025-01-26 12:26:39 +0530

    client proto file created

---
+++ proto/randomPackage/InitiateRequest.ts
@@ -0,0 +1,12 @@
+// Original file: proto/random.proto
+
+
+export interface InitiateRequest {
+  'name'?: (string);
+  'avataarUrl'?: (string);
+}
+
+export interface InitiateRequest__Output {
+  'name'?: (string);
+  'avataarUrl'?: (string);
+}
--- server.ts
+++ server.ts
@@ -2,11 +2,8 @@ import path from 'path'
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
 import {ProtoGrpcType} from './proto/random'
-import { RandomHandlers } from './proto/randomPackage/Random'
-import { TodoResponse } from './proto/randomPackage/TodoResponse'
-import { TodoRequest } from './proto/randomPackage/TodoRequest'
-import { ChatRequest } from './proto/randomPackage/ChatRequest'
-import { ChatResponse } from './proto/randomPackage/ChatResponse'
+import { ChatServiceHandlers } from './proto/randomPackage/ChatService'
+
 
 const PORT = 8082
 const PROTO_FILE = './proto/random.proto'
@@ -29,83 +26,20 @@ function main() {
   })
 }
 
-const todoList: TodoResponse = {todos :[] }
-const callObjByUsername = new Map<string, grpc.ServerDuplexStream<ChatRequest, ChatResponse>>()
+// const callObjByUsername = new Map<string, grpc.ServerDuplexStream<ChatRequest, ChatResponse>>()
 
 function getServer() {
   const server = new grpc.Server()
-  server.addService(randomPackage.Random.service, {
-    PingPong: (req, res) => {
-      console.log(req.request)
-      res(null, {message: "Pong"})
-    },
-    RandomNumbers: (call) => {
-      const { maxVal = 10 } = call.request
-      console.log({maxVal})
-
-      let runCount = 0
-      const id = setInterval(() => {
-        runCount = ++runCount
-        call.write({num: Math.floor(Math.random() * maxVal)})
-
-        if(runCount >= 10) {
-          clearInterval(id)
-          call.end()
-        }
-      }, 500)
-    },
-    TodoList: (call, callback) => {
-      call.on("data", (chunk: TodoRequest) => {
-        todoList.todos?.push(chunk)
-        console.log(chunk)
-      })
-
-      call.on("end", () => {
-        callback(null, {todos: todoList.todos})
-      })
-    },
-    Chat: (call) => {
-      call.on("data", (req) => {
-        const username = call.metadata.get('username')[0] as string
-        const msg = req.message
-        console.log(username, req.message)
-
-
-        for(let [user, usersCall] of callObjByUsername) {
-          if(username !== user) {
-            usersCall.write({
-              username: username,
-              message: msg
-            })
-          }
-        }
-
-        if (callObjByUsername.get(username) === undefined) {
-          callObjByUsername.set(username, call)
-        }
-      })
-
-      call.on("end", () => {
-        const username = call.metadata.get('username')[0] as string
-        callObjByUsername.delete(username)
-        for(let [user, usersCall] of callObjByUsername) {
-            usersCall.write({
-              username: username,
-              message: "Has Left the Chat!"
-            })
-        }
-        console.log(`${username} is ending their chat session`)
-
-        call.write({
-          username: "Server",
-          message: `See you later ${username}`
-        })
-
-        call.end()
-      })
-
+  server.addService(randomPackage.ChatService.service, {
+    ChatInitiate: (call,callback) => {
+      const sessionName= call.request.name || '';
+      const avataar= call.request.avataarUrl || '';
+      if(!sessionName || !avataar) return callback( new Error('Name and Avataar are required'), null) 
+      
+      callback(null,{id:Math.floor(Math.random() * 10000)})
+      
     }
-  } as RandomHandlers)
+  } as ChatServiceHandlers)
 
   return server
 }
